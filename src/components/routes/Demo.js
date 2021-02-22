25b15fb041ccb908df3b77ab8f9354e7d92ba382
immxalan <aedocoding@gmail.com>
2021-02-22 11:58:04 -0800

    Fixed Youtube embed link

--- src/components/routes/Demo.js
+++ src/components/routes/Demo.js
@@ -6,7 +6,7 @@ export default function Demo (props) {
   return (
     <React.Fragment>
       <Grid className = "fade" container justify="center" style={{marginTop: 100}}>
-      <iframe title="demo" width="560" height="315" src="https://youtu.be/c3CRF2iSyvA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe title="demo" width="560" height="315" src="https://www.youtube.com/embed/c3CRF2iSyvA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </Grid>
     </React.Fragment>
   );
